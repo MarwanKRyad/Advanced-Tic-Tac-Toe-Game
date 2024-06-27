a51852dcc692103ededadae59ef764762ad62cac
Marwan Khaled Mohamed Ryad <163741736+MarwanKRyad@users.noreply.github.com>
2024-06-27 14:41:52 +0300

    Add files via upload

---
+++ codeBase/main.cpp
@@ -0,0 +1,23 @@
+#include "mainwindow.h"
+
+#include <QApplication>
+#include <QtSql>
+#include <QDebug>
+#include <chrono>
+int main(int argc, char *argv[])
+{
+    QApplication a(argc, argv);
+
+    QSqlDatabase tic=QSqlDatabase::addDatabase("QSQLITE");;
+    tic.setDatabaseName("C:/Qt/users_.db");
+    qDebug()<<"problem";
+
+    if(!tic.open())
+    {
+        qDebug()<<"problem";
+    }
+
+    MainWindow w;
+    w.show();
+    return a.exec();
+}
